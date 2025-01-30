ef9b841efe096fc5c9408a82229979b0bfc6f056
cxrloosz <ricippe2707@gmail.com>
2025-01-30 18:19:36 -0300

    Update README.lua

--- README.lua
+++ README.lua
@@ -16,34 +16,68 @@ local Tabs = {
 -- Descrição
 Tabs.Main:AddParagraph({ Title = "AFE", Content = "CxrloScript" })
 
--- Variável para controle do loop
+-- Variáveis para controle dos loops
 local endlessEnabled = false
+local cardFuseEnabled = false
 
--- Função para executar o código quando ativado
-local function executeScript()
-    while endlessEnabled and task.wait(1) do -- Loop enquanto ativado
+-- Função para executar o Endless
+local function executeEndless()
+    while endlessEnabled and task.wait(1) do
         local args = {
             [1] = "/\229\133\179\229\141\161\231\179\187\231\187\159/\229\133\179\229\141\161\229\149\134\228\186\186/\230\140\145\230\136\152\228\184\187\231\186\191?\232\180\173\228\185\176",
             [2] = "/\229\133\179\229\141\161\231\179\187\231\187\159/\228\184\187\231\186\191\229\133\179\229\141\161/\228\184\187\231\186\191\229\156\186\230\153\1759/\228\184\187\231\186\191\229\156\186\230\153\1759-5"
         }
 
         game:GetService("ReplicatedStorage").Packages.WuKong.RemoteActionFunction:InvokeServer(unpack(args))
-        print("Script executado")
+        print("Endless Executado")
     end
 end
 
--- Adicionando o Toggle na interface
+-- Função para executar o CardFuse
+local function executeCardFuse()
+    while cardFuseEnabled and task.wait(1) do
+        local args = {
+            [1] = "/\229\141\161\231\137\140\231\179\187\231\187\159/\229\141\161\231\137\140\229\149\134\228\186\186/\232\158\141\229\144\136\229\141\161\231\137\140?\232\180\173\228\185\176",
+            [2] = "/\229\141\161\231\137\140\231\179\187\231\187\159/\229\141\161\231\137\140\232\131\140\229\140\133/\229\191\151\230\157\145\230\150\176\229\133\171",
+            [3] = "null",
+            [4] = {
+                [1] = -30
+            }
+        }
+
+        game:GetService("ReplicatedStorage").Packages.WuKong.RemoteActionFunction:InvokeServer(unpack(args))
+        print("CardFuse Executado")
+    end
+end
+
+-- Toggle para ativar/desativar o Endless
 Tabs.Main:AddToggle("EndlessToggle", {
     Title = "Endless",
-    Description = "Liga/desliga o script automaticamente",
+    Description = "Liga/desliga o Endless automaticamente",
     Default = false,
     Callback = function(state)
         endlessEnabled = state -- Ativa ou desativa
         if endlessEnabled then
-            print("Endless Script Ligado!")
-            task.spawn(executeScript) -- Inicia a execução
+            print("Endless Ligado!")
+            task.spawn(executeEndless) -- Inicia a execução
         else
-            print("Endless Script Desligado!")
+            print("Endless Desligado!")
+        end
+    end
+})
+
+-- Toggle para ativar/desativar o CardFuse
+Tabs.Main:AddToggle("CardFuseToggle", {
+    Title = "CardFuse",
+    Description = "Liga/desliga o CardFuse automaticamente",
+    Default = false,
+    Callback = function(state)
+        cardFuseEnabled = state -- Ativa ou desativa
+        if cardFuseEnabled then
+            print("CardFuse Ligado!")
+            task.spawn(executeCardFuse) -- Inicia a execução
+        else
+            print("CardFuse Desligado!")
         end
     end
 })
