a82e6660a32a00a1eaaabf3553398b681a6c4790
cxrloosz <ricippe2707@gmail.com>
2025-01-30 18:29:22 -0300

    Update README.lua

--- README.lua
+++ README.lua
@@ -16,13 +16,13 @@ local Tabs = {
 -- Descrição
 Tabs.Main:AddParagraph({ Title = "AFE", Content = "CxrloScript" })
 
--- Variáveis para controle dos loops
+-- Variáveis de controle
 local endlessEnabled = false
 local cardFuseEnabled = false
 
 -- Função para executar o Endless
 local function executeEndless()
-    while endlessEnabled and task.wait(1) do
+    while endlessEnabled do
         local args = {
             [1] = "/\229\133\179\229\141\161\231\179\187\231\187\159/\229\133\179\229\141\161\229\149\134\228\186\186/\230\140\145\230\136\152\228\184\187\231\186\191?\232\180\173\228\185\176",
             [2] = "/\229\133\179\229\141\161\231\179\187\231\187\159/\228\184\187\231\186\191\229\133\179\229\141\161/\228\184\187\231\186\191\229\156\186\230\153\1759/\228\184\187\231\186\191\229\156\186\230\153\1759-5"
@@ -30,12 +30,13 @@ local function executeEndless()
 
         game:GetService("ReplicatedStorage").Packages.WuKong.RemoteActionFunction:InvokeServer(unpack(args))
         print("Endless Executado")
+        wait(1) -- Evita sobrecarga
     end
 end
 
 -- Função para executar o CardFuse
 local function executeCardFuse()
-    while cardFuseEnabled and task.wait(1) do
+    while cardFuseEnabled do
         local args = {
             [1] = "/\229\141\161\231\137\140\231\179\187\231\187\159/\229\141\161\231\137\140\229\149\134\228\186\186/\232\158\141\229\144\136\229\141\161\231\137\140?\232\180\173\228\185\176",
             [2] = "/\229\141\161\231\137\140\231\179\187\231\187\159/\229\141\161\231\137\140\232\131\140\229\140\133/\229\191\151\230\157\145\230\150\176\229\133\171",
@@ -47,11 +48,12 @@ local function executeCardFuse()
 
         game:GetService("ReplicatedStorage").Packages.WuKong.RemoteActionFunction:InvokeServer(unpack(args))
         print("CardFuse Executado")
+        wait(1) -- Evita sobrecarga
     end
 end
 
 -- Toggle para ativar/desativar o Endless
-local EndlessToggle = Tabs.Main:AddToggle("EndlessToggle", {
+Tabs.Main:AddToggle("EndlessToggle", {
     Title = "Endless",
     Description = "Liga/desliga o Endless automaticamente",
     Default = false,
@@ -67,7 +69,7 @@ local EndlessToggle = Tabs.Main:AddToggle("EndlessToggle", {
 })
 
 -- Toggle para ativar/desativar o CardFuse
-local CardFuseToggle = Tabs.Main:AddToggle("CardFuseToggle", {
+Tabs.Main:AddToggle("CardFuseToggle", {
     Title = "CardFuse",
     Description = "Liga/desliga o CardFuse automaticamente",
     Default = false,
